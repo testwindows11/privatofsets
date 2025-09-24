1e39a1aade64dce2ea99482986e6faa2b4941e2d
testwindows11 <jogogo036@gmail.com>
2025-09-24 13:59:56 +0300

    Delete vec_normalize.py

--- vec_normalize.py
+++
@@ -1,8 +0,0 @@
-# vec_normalize.py
-# -*- coding: utf-8 -*-
-from vec_length import vec_length
-def vec_normalize(vec):
-    length = vec_length(vec)
-    if length == 0:
-        return [0,0,0]
-    return [x / length for x in vec]
