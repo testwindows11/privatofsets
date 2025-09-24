2ada6ca694b1b54ad60304007026fdc2309b6bfd
testwindows11 <jogogo036@gmail.com>
2025-09-24 13:58:43 +0300

    Delete is_process_running.py

--- is_process_running.py
+++
@@ -1,8 +0,0 @@
-# is_process_running.py
-# -*- coding: utf-8 -*-
-def is_process_running(pm, process_name):
-    try:
-        _ = pm.process_handle
-        return True
-    except Exception:
-        return False
